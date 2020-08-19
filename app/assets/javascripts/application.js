dea903a5e652a28f1dde6e78519c4f717fb8504a
gameplan1975 <gameplan1975@gmail.com>
2020-08-19 18:55:58 +0900

    うんちくが表示されないバグを修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,7 @@
 //= require activestorage
 //= require turbolinks
 //= require jquery3
-// require popper
-// require bootstrap-sprockets
+//= require popper
+//= require bootstrap-sprockets
 //= require sortablejs/Sortable.js
 //= require_tree .
